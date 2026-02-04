5da32dfab58179efaaff14fe4c87e39b7e6337a5
JohnnyChen1113 <1020607557@qq.com>
2026-02-04 17:17:13 -0600

    Initial Commit

---
+++ AIPaperReader/AIPaperReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIPaperReaderApp.swift
+//  AIPaperReader
+//
+//  Created by JohnnyChan on 2/4/26.
+//
+
+import SwiftUI
+
+@main
+struct AIPaperReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIPaperReaderTests/AIPaperReaderTests.swift
@@ -0,0 +1,17 @@
+//
+//  AIPaperReaderTests.swift
+//  AIPaperReaderTests
+//
+//  Created by JohnnyChan on 2/4/26.
+//
+
+import Testing
+@testable import AIPaperReader
+
+struct AIPaperReaderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
